fce8a7ee0a64a4f901d47b770d63f7aefdaa4bf1
Ibrahim bayana <ibrahimbayana@gmail.com>
2025-03-06 21:46:26 +0100

    duplicateRemove2

--- src/Duplicate2.java
+++ src/Duplicate2.java
@@ -1,2 +1,24 @@
+import java.util.LinkedHashSet;
+
 public class Duplicate2 {
+
+    public static void main(String[] args) {
+
+
+        String input = "AABACCDDA";
+        removeDuplicates(input);
+    }
+
+    public static void removeDuplicates(String str) {
+        LinkedHashSet<Character> uniqueChars = new LinkedHashSet<>();
+
+        for (char c : str.toCharArray()) {
+            uniqueChars.add(c);
+        }
+
+        // Convert set back to a string and print
+        for (char c : uniqueChars) {
+            System.out.print(c);
+        }
+    }
 }
